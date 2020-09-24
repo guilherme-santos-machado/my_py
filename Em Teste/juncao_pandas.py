981088e6e3f1a2aa3b15e972e7ff690dd043b2ba
Guilherme Machado <machado.guilherme@live.com>
2020-09-24 13:15:01 -0300

    file update

--- Em Teste/juncao_pandas.py
+++
@@ -1,7 +0,0 @@
-# import pandas as pd
-
-# #change xxx with the sheet name that includes the data
-# data = pd.read_Excel(sourcefile, sheet_name="xxx")
-
-# #save it to the 'new_tab' in destfile
-# data.to_Excel(destfile, sheet_name='new_tab')
--- Em Teste/novo.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-
-df = pd.read_csv('./teste.csv', low_memory=False, header = 1)
-
-df.to_csv('teste2.csv')
